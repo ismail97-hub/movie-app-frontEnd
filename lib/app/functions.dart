36a94ab539080bec4533ac55d84f5f7daf6200dc
ismail97-hub <ismailbelkadi4@gmail.com>
2022-02-23 05:36:56 +0100

    add permission

--- lib/app/functions.dart
+++ lib/app/functions.dart
@@ -1,37 +1,43 @@
-
 import 'dart:io';
 import 'package:device_information/device_information.dart';
 import 'package:intl/intl.dart' as intl;
 import 'package:device_info/device_info.dart';
 import 'package:flutter/services.dart';
 import 'package:movieapp/domain/model/model.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 Future<DeviceInfo> getDeviceDetails() async {
   String name = "Unknown";
   String identifier = "Unknown";
-
-  // DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
 
   try {
     if (Platform.isAndroid) {
-      // return android device info
-      // var build = await deviceInfoPlugin.androidInfo;
-      // name = build.brand + " " + build.model;
-      name = await DeviceInformation.deviceName;
+      var build = await deviceInfoPlugin.androidInfo;
+      name = build.brand + " " + build.model;
       identifier = await DeviceInformation.deviceIMEINumber;
     } else if (Platform.isIOS) {
-      // // return ios device info
-      // var build = await deviceInfoPlugin.iosInfo;
-      // name = build.name + " " + build.model;
-      // identifier = build.identifierForVendor;
+      var build = await deviceInfoPlugin.iosInfo;
+      name = build.name + " " + build.model;
+      identifier = build.identifierForVendor;
     }
   } on PlatformException {
-    // return default data here
     return DeviceInfo(name, identifier);
   }
   return DeviceInfo(name, identifier);
 }
 
+Future<bool> isPhonePermissionGaranted()async {
+  await [Permission.phone].request();
+  final PermissionStatus permission = await Permission.phone.status;
+  if (permission == PermissionStatus.granted) {
+    return Future.value(true);
+  }
+  else{
+    return Future.value(false);
+  }
+}
+
 bool isRTL(String text) {
-    return intl.Bidi.detectRtlDirectionality(text);
+  return intl.Bidi.detectRtlDirectionality(text);
 }
